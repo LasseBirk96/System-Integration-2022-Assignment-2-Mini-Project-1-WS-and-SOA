d73ed9a7ecc27fc7b02647eda52569d8c9603e27
Lasse Birk <lassebirk@Lasses-MacBook-Pro.local>
2022-09-14 17:32:11 +0200

    Assignment done

---
+++ System-Integration-A2/System-Integration-A2/PhoneCodeSOAPService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Xml.Linq;
+
+namespace System_Integration_A2
+{
+    public class PhoneCodeSOAPService
+    {
+        private String url;
+
+        public PhoneCodeSOAPService(String url = "http://webservices.oorsprong.org/websamples.countryinfo/CountryInfoService.wso")
+        {
+            this.url = url;
+        }
+
+        public async Task<String> GetPhoneCodeExtensionByCountryCode(String countryCode)
+        {
+            var body =
+                string.Format(@"<?xml version=""1.0"" encoding=""utf-8""?>
+                <soap:Envelope xmlns:soap=""http://schemas.xmlsoap.org/soap/envelope/"">
+                  <soap:Body>
+                    <CountryIntPhoneCode xmlns=""http://www.oorsprong.org/websamples.countryinfo"">
+                      <sCountryISOCode>{0}</sCountryISOCode>
+                    </CountryIntPhoneCode>
+                  </soap:Body>
+                </soap:Envelope>", countryCode);
+            var client = new HttpClient();
+            var request = new StringContent(body);
+            request.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("text/xml");
+            var response = await client.PostAsync(this.url, request);
+
+            return this.Parse(await response.Content.ReadAsStringAsync());
+        }
+
+        private String Parse(String response)
+        {
+            var xdoc = XDocument.Parse(response);
+            XNamespace ns = "http://www.oorsprong.org/websamples.countryinfo";
+            var element = xdoc.Descendants(ns + "CountryIntPhoneCodeResult").FirstOrDefault()?.Value;
+            return element ?? "";
+        }
+    }
+}
+
--- System-Integration-A2/System-Integration-A2/Program.cs
+++ System-Integration-A2/System-Integration-A2/Program.cs
@@ -1,42 +1,40 @@
-﻿
-using System.Net;
-using Webserver;
+﻿using System.Net;
 using Tiger;
 using GenderTiger;
-using System_Integration_A2.Entities;
+using System_Integration_A2;
 
 
-
-internal class Program {
-
-    public static string SendResponse(HttpListenerRequest request)
+internal class Program
+{
+    public static async Task<String> GenerateInvitationBasedOnUserInformation(string name, string ip)
     {
-        return string.Format("<HTML><BODY>wrgoerbgrihriuhgb.<br>{0}</BODY></HTML>", DateTime.Now);
+            var title = "";
+            var api = new GenderApi("LQ3YexQLXBmFjXgU5LvBS5hGBPUjTNAcVqUX");
+            var IpData = new IpInfo();
+            var countryCode = IpData.GetCountryCodeOnIP(ip);
+            var gender = await api.GetGenderByName(name, countryCode);
+            if (gender is "male") {
+                title = "Mr.";
+            }
+            if (gender is "female") {
+                title = "Ms.";
+            }
+            if (gender is not "male" or "female") {
+            title = "";
+            }
+
+            var soapAPI = new PhoneCodeSOAPService();
+            var extension = await soapAPI.GetPhoneCodeExtensionByCountryCode(countryCode);
+            string invitation = String.Format("Dear {0}{1},\nYou are hereby invited to the annual sales report meeting. " +
+            "This year has been great for the company, and attached you can see this years report.\n" +
+            "If you can't make it to the event please call this number to cancel: +{2} 1234567.", title, name, extension);
+            return invitation;
     }
 
-    
-    static async Task Main(string[] args)
-    {
-        /*
-        var person = new Person("Lars", "lars@gmail.com", "194.62.169.90");
-        var getIp = new IpInfo();
-        var personCountryCode= getIp.GetCountryCodeOnIP(person.IP);*/
 
-        // LQ3YexQLXBmFjXgU5LvBS5hGBPUjTNAcVqUX
-
-        var api = new GenderApi("LQ3YexQLXBmFjXgU5LvBS5hGBPUjTNAcVqUX");
-        Console.WriteLine(await api.GetGenderByName("Frank Nielsen"));
-        /*
-        var ws = new WebServer(SendResponse, "http://localhost:8080/test/");
-        ws.Run();
-        Console.WriteLine("A simple webserver. Press a key to quit.");
-        Console.ReadKey();
-        ws.Stop();*/
-    }
+       static async Task Main(string[] args) {
+       // This can then be repeated for a list of people, whoever this is just to provide proof of concept
+       var invitation = await GenerateInvitationBasedOnUserInformation("Frank Nielsen", "195.215.220.254");
+       Console.WriteLine(invitation);
+       }
 }
-
-
-/*
- Liste af dudes via SOAP
-   
-*/
